da8929eac71f21a48947f30e1bb835ad7393b170
Travis Burns <travisjb0@gmail.com>
2021-03-23 16:57:22 -0700

    Test 2 of commit

---
+++ js/index.js
@@ -0,0 +1,57 @@
+
+
+
+/*animate on scroll initialise script */
+
+
+$(function() {
+    AOS.init();
+  });
+  
+  $(window).on('load', function() {
+    AOS.refresh();
+  });
+
+/*isotope script */
+
+$(document).ready(function(){
+    let $btns=$('.project-area .button-group button');
+
+    $btns.click(function(e){
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+           filter:selector 
+        });
+
+        return false;
+    })
+
+
+    /* Modal Controls */
+
+    
+
+
+/* Sticky Navigation On Scroll */
+
+let nav_offset_top=$('.header_area').height()+ 50;
+
+function navbarFixed(){
+    if($('.header_area').length){
+        $(window).scroll(function(){
+            let scroll=$(window).scrollTop();
+            if(scroll>=nav_offset_top){
+                $('.header_area .main-menu').addClass('navbar_fixed');
+            }else{
+                $('.header_area .main-menu').removeClass('navbar_fixed');
+            }
+        })
+    }
+}
+
+navbarFixed();
+
+});
